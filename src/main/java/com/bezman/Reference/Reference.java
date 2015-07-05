d5aa47c6650c5c180d4036d88b105194dac3bd54
Terence Bezman <terencebezman@gmail.com>
2015-07-05 13:13:37 -0400

    Updated OAuth callback

--- src/main/java/com/bezman/Reference/Reference.java
+++ src/main/java/com/bezman/Reference/Reference.java
@@ -22,8 +22,8 @@ import java.sql.*;
  */
 public class Reference
 {
-//        public static String rootURL = "http://devwars.tv";
-    public static String rootURL = "http://bezcode.com:9090";
+        public static String rootURL = "http://devwars.tv";
+//    public static String rootURL = "http://bezcode.com:9090";
 //    public static String rootURL = "http://localhost:8080";
     public static String[] allowedHosts = new String[]{"localhost:8080", "localhost:81", "devwars.tv", "bezcode.com:9090"};
 
