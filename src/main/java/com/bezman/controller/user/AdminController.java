6cee8a74aab1a6ae0df9de840c286edf81493f65
Terence Bezman <terencebezman@gmail.com>
2015-08-02 21:59:00 -0400

    Added photo list json

--- src/main/java/com/bezman/controller/user/AdminController.java
+++ src/main/java/com/bezman/controller/user/AdminController.java
@@ -4,6 +4,8 @@ import com.bezman.Reference.Reference;
 import com.bezman.Reference.util.Util;
 import com.bezman.annotation.PreAuthorization;
 import com.bezman.model.User;
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +43,38 @@ public class AdminController
         zipOutputStream.close();
 
         return null;
+    }
+
+    @PreAuthorization(minRole = User.Role.ADMIN)
+    @RequestMapping("/photoarchivelist")
+    public ResponseEntity getArchiveFolderList(HttpServletResponse response) throws IOException
+    {
+        File zipDir = new File(Reference.PROFILE_PICTURE_PATH_NO_END);
+//        File zipDir = new File("C:\\Users\\Terence\\IdeaProjects\\DevWars Maven\\src\\main\\java");
+
+        JSONObject jsonObject = new JSONObject();
+
+        addFolderToJSONObject(zipDir, jsonObject);
+
+        return new ResponseEntity(jsonObject, HttpStatus.OK);
+    }
+
+    private void addFolderToJSONObject(File dir, JSONObject jsonObject)
+    {
+        for(File file : dir.listFiles())
+        {
+            if (file.isDirectory())
+            {
+                JSONObject newDir = new JSONObject();
+
+                addFolderToJSONObject(file, newDir);
+
+                jsonObject.put(file.getName(), newDir);
+            } else
+            {
+                jsonObject.put(file.getName(), file.getName());
+            }
+        }
     }
 
 
