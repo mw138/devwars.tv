da9e1cb469c729e6eb11fe32b69203acb21de86d
= <terencebezman@gmail.com>
2015-09-17 18:33:03 -0400

    removed concat from js watch

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -188,8 +188,8 @@ gulp.task('serve', ['styles'], function () {
 
 gulp.task('watch-files', function(){
     gulp.watch(['app/**/*.html'], reload);
-    gulp.watch(['app/**/*.js'], ['concat', reload]);
-    gulp.watch(['app/**/**/*.js'], ['concat', reload]);
+    gulp.watch(['app/**/*.js'], [reload]);
+    gulp.watch(['app/**/**/*.js'], [reload]);
     gulp.watch(['assets/sass/**/*.scss'], ['styles', reload]);
     gulp.watch(['app/images/**/*'], reload);
 });
