311a5cc2d1076ca18660034875cc4e22dda354b6
Steven Psomas <psomas92@gmail.com>
2015-10-29 23:07:44 -0700

    Added the 'Forgot password' link to dialog

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -83,7 +83,7 @@ app.config(['$urlRouterProvider', '$httpProvider', '$locationProvider', function
     // all page specific routes are in their js file
     $urlRouterProvider.otherwise('/');
 
-    if(false) {
+    if(true) {
         $httpProvider.interceptors.push(function () {
 
             var Interceptor = {};
