4eb1afc6cdce4d838745b19c7139cd9f4f6e336d
= <terencebezman@gmail.com>
2016-01-25 14:50:41 -0500

    Removed useless security methods

--- src/main/java/com/bezman/service/Security.java
+++ src/main/java/com/bezman/service/Security.java
@@ -2,8 +2,6 @@ package com.bezman.service;
 
 import com.bezman.Reference.Reference;
 import org.springframework.stereotype.Service;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
@@ -31,53 +29,6 @@ public class Security {
             }
 
             return stringBuilder.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public String encrypt(String item) {
-        try {
-            String encryptionKey = Reference.getEnvironmentProperty("encryptionKey");
-
-            DESKeySpec keySpec = new DESKeySpec(encryptionKey.getBytes());
-            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-
-            SecretKey key = keyFactory.generateSecret(keySpec);
-
-            BASE64Encoder base64Encoder = new BASE64Encoder();
-
-            byte[] clearBytes = item.getBytes("UTF8");
-            Cipher cipher = Cipher.getInstance("DES");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-
-            return base64Encoder.encode(cipher.doFinal(clearBytes));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public String decrypt(String item) {
-        try {
-            String encryptionKey = Reference.getEnvironmentProperty("encryptionKey");
-
-            DESKeySpec keySpec = new DESKeySpec(encryptionKey.getBytes());
-            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-
-            SecretKey key = keyFactory.generateSecret(keySpec);
-
-            BASE64Decoder base64Decoder = new BASE64Decoder();
-
-            byte[] encryptedBytes = base64Decoder.decodeBuffer(item);
-
-            Cipher cipher = Cipher.getInstance("DES");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-
-            return new String(cipher.doFinal(encryptedBytes), "UTF8");
         } catch (Exception e) {
             e.printStackTrace();
         }
